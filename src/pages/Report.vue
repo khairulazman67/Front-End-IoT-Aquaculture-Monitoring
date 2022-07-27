587b4259b3edf59980cf78fad069174a8c6a8093
khairulazman67 <azman.khairul67@gmail.com>
2022-07-27 22:04:49 +0700

    update

--- src/pages/Report.vue
+++ src/pages/Report.vue
@@ -94,6 +94,98 @@
 
                         </div>
                     </div>
+                    
+                    <div class="w-1/3 xs:w-full h-full bg-white shadow-xl mr-2 rounded-3xl flex flex-col ">
+                        <div class="h-14 rounded-t-3xl bg-secondary-900 px-4 py-4 text-lg font-bold text-white">
+                            <div>Data Sensor Temperatur</div>
+                        </div>
+                        <div class="px-4 py-4 h-auto flex justify-center">
+                            <div class="overflow-y-auto overflow-x-auto h-96 shadow-md sm:rounded-lg bg-gray-600">
+                                <table class="w-auto text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" class="py-3 px-6">
+                                                No
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Nama
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Value
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Waktu
+                                            </th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr  v-for="(c,i) in dataTemperatur" :key="i" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <th scope="row" class="text-center  font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {{i+1}}
+                                            </th>
+                                            <td class="py-2 px-2">
+                                                Temperatur
+                                            </td>
+                                            <td class="py-2 px-2 text-center">
+                                                {{c.value}}
+                                            </td>
+                                            <td class="py-2 px-2">
+                                                {{date_format(c.createdAt)}}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="w-1/3 xs:w-full h-full bg-white shadow-xl mr-2 rounded-3xl flex flex-col ">
+                        <div class="h-14 rounded-t-3xl bg-secondary-900 px-4 py-4 text-lg font-bold text-white">
+                            <div>Data Sensor pH</div>
+                        </div>
+                        <div class="px-4 py-4 h-auto flex justify-center">
+                            <div class="overflow-y-auto overflow-x-auto h-96 shadow-md sm:rounded-lg bg-gray-600">
+                                <table class="w-auto text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th scope="col" class="py-3 px-6">
+                                                No
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Nama
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Value
+                                            </th>
+                                            <th scope="col" class="py-3 px-6">
+                                                Waktu
+                                            </th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr  v-for="(c,i) in datapH" :key="i" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <th scope="row" class="text-center  font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {{i+1}}
+                                            </th>
+                                            <td class="py-2 px-2">
+                                                Turbidity
+                                            </td>
+                                            <td class="py-2 px-2 text-center">
+                                                {{c.value}}
+                                            </td>
+                                            <td class="py-2 px-2">
+                                                {{date_format(c.createdAt)}}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
         </div>
     </div>
